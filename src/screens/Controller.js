959e56abd755c94fb2db1bd20ee337e9534b5371
sagar1976 <narvekar.sagar@gmail.com>
2019-10-11 12:19:53 +0530

    Updated IP
    update ip

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -10,7 +10,7 @@ class Controller extends Component {
   constructor()
   {
     super();
-    this.baseUrl = "http://3.95.132.174:8080/api/v1/";
+    this.baseUrl = "http://35.175.246.169:8080/api/v1/";
   }
   render(){
     return(
